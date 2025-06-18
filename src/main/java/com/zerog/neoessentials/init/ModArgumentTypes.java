5866766c98fe61c80e82974df6bb3be5f6b860c0
MrWhiteFlamesYT <swiegersjj1@gmail.com>
2025-06-18 13:26:36 +0200

    feat: Add custom command argument types for string to boolean conversion

--- src/main/java/com/zerog/neoessentials/init/ModArgumentTypes.java
+++ src/main/java/com/zerog/neoessentials/init/ModArgumentTypes.java
@@ -1,6 +1,7 @@
 package com.zerog.neoessentials.init;
 
 import com.zerog.neoessentials.NeoEssentials;
+<<<<<<< HEAD
 import com.zerog.neoessentials.utils.VanillaBooleanParser;
 
 import net.neoforged.bus.api.IEventBus;
@@ -42,5 +43,36 @@ public class ModArgumentTypes {
             
             NeoEssentials.LOGGER.info("Vanilla-compatible command system configured successfully");
         });
+=======
+import com.zerog.neoessentials.utils.StringToBooleanArgumentType;
+import com.zerog.neoessentials.utils.StringToBooleanArgumentInfo;
+
+import net.minecraft.commands.synchronization.ArgumentTypeInfo;
+import net.minecraft.commands.synchronization.ArgumentTypeInfos;
+import net.minecraft.core.registries.Registries;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+/**
+ * Handles registration of custom argument types
+ */
+public class ModArgumentTypes {
+    // Create a deferred register for command argument types
+    public static final DeferredRegister<ArgumentTypeInfo<?, ?>> COMMAND_ARGUMENT_TYPES = 
+            DeferredRegister.create(Registries.COMMAND_ARGUMENT_TYPE, NeoEssentials.MODID);
+
+    // Register our StringToBooleanArgumentType with the StringToBooleanArgumentInfo
+    static {
+        ArgumentTypeInfos.registerByClass(StringToBooleanArgumentType.class, new StringToBooleanArgumentInfo());
+    }
+
+    /**
+     * Register this class with the mod event bus to enable the registrations
+     * 
+     * @param eventBus The mod event bus
+     */
+    public static void register(net.neoforged.bus.api.IEventBus eventBus) {
+        NeoEssentials.LOGGER.info("Registering custom command argument types");
+        COMMAND_ARGUMENT_TYPES.register(eventBus);
+>>>>>>> 8a36c07 (feat: Add custom command argument types for string to boolean conversion)
     }
 }
--- src/main/java/com/zerog/neoessentials/utils/StringToBooleanArgumentInfo.java
+++ src/main/java/com/zerog/neoessentials/utils/StringToBooleanArgumentInfo.java
@@ -1,5 +1,6 @@
 package com.zerog.neoessentials.utils;
 
+<<<<<<< HEAD
 /**
  * @deprecated This class has been replaced by the server-side only approach in {@link VanillaBooleanParser}.
  * This file is kept for reference only and should be removed in future versions.
@@ -11,4 +12,56 @@ package com.zerog.neoessentials.utils;
 public class StringToBooleanArgumentInfo {
     // This class has been intentionally emptied and deprecated.
     // The VanillaBooleanParser approach does not require ArgumentTypeInfo registration.
+=======
+import com.mojang.brigadier.arguments.ArgumentType;
+import net.minecraft.commands.CommandBuildContext;
+import net.minecraft.commands.synchronization.ArgumentTypeInfo;
+import net.minecraft.network.FriendlyByteBuf;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Info class for StringToBooleanArgumentType, needed to register it with the command system
+ */
+public class StringToBooleanArgumentInfo implements ArgumentTypeInfo<StringToBooleanArgumentType, StringToBooleanArgumentInfo.Template> {
+
+    @Override
+    public void serializeToNetwork(@Nonnull Template template, @Nonnull FriendlyByteBuf buffer) {
+        // No additional data needed for this argument type
+    }
+
+    @Override
+    @Nonnull
+    public Template deserializeFromNetwork(@Nonnull FriendlyByteBuf buffer) {
+        return new Template();
+    }
+
+    @Override
+    public void serializeToJson(@Nonnull Template template, @Nonnull com.google.gson.JsonObject json) {
+        // No additional data needed for this argument type
+    }
+
+    @Override
+    @Nonnull
+    public Template unpack(@Nonnull StringToBooleanArgumentType argumentType) {
+        return new Template();
+    }
+
+    /**
+     * Template class for StringToBooleanArgumentType
+     */
+    public class Template implements ArgumentTypeInfo.Template<StringToBooleanArgumentType> {
+        @Override
+        @Nonnull
+        public StringToBooleanArgumentType instantiate(@Nonnull CommandBuildContext context) {
+            return StringToBooleanArgumentType.stringToBoolean();
+        }
+
+        @Override
+        @Nonnull
+        public ArgumentTypeInfo<StringToBooleanArgumentType, ?> type() {
+            return StringToBooleanArgumentInfo.this;
+        }
+    }
+>>>>>>> 8a36c07 (feat: Add custom command argument types for string to boolean conversion)
 }
