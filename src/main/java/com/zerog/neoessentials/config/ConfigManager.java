eb7a2b1e7362e029750648460fd61fe972a6f007
MrWhiteFlamesYT <swiegersjj1@gmail.com>
2026-05-13 20:37:06 +0200

    ✨ Implement in-game Discord registration via OAuth2 for streamlined user onboarding

--- src/main/java/com/zerog/neoessentials/config/ConfigManager.java
+++ src/main/java/com/zerog/neoessentials/config/ConfigManager.java
@@ -1282,7 +1282,7 @@ public class ConfigManager {
         put(ECONOMY_CONFIG, 2);
         put(PERMISSIONS_CONFIG, 5);
         put(KITS_CONFIG, 1);
-        put(DISCORD_AUTH_CONFIG, 6);
+        put(DISCORD_AUTH_CONFIG, 7);
         put(TABLIST_CONFIG, 2);
     }};
 
